2f4b69b67b31d4d1560e2e274aba1184baf471b3
Rafael Garcia <rafbgarcia@gmail.com>
2018-01-22 20:39:21 -0500

    Working on another PC

--- apps/db/config/dev.exs
+++ apps/db/config/dev.exs
@@ -5,6 +5,6 @@ config :db, Db.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "rafa",
   password: "",
-  database: "my_app_dev",
+  database: "imoto_dev",
   hostname: "localhost",
   pool_size: 10
