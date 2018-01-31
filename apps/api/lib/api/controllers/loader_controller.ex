2b6b6fdc7aa61d2435891da7a32a4abb42a3d583
Rafael Garcia <rafbgarcia@gmail.com>
2018-01-31 15:26:05 -0200

    loader challenge

--- apps/api/lib/api/controllers/loader_controller.ex
+++ apps/api/lib/api/controllers/loader_controller.ex
@@ -2,7 +2,7 @@ defmodule Api.Controllers.LoaderController do
   use Api, :controller
 
   def challenge(conn, _) do
-    send_resp(conn, 200, "loaderio-ccfc38fc49ce0360c1f93b82d8ecf647")
+    send_resp(conn, 200, "loaderio-8b5de0077a6b032f6f821be422161762")
   end
 
 end
