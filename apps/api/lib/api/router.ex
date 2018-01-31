4214a98bacd7e37c7a98cfceb18b9587a6775f7d
Rafael Garcia <rafbgarcia@gmail.com>
2018-01-31 15:10:48 -0200

    Terms of use route

--- apps/api/lib/api/router.ex
+++ apps/api/lib/api/router.ex
@@ -2,17 +2,7 @@ defmodule Api.Router do
   use Api, :router
 
   get "/loaderio-ccfc38fc49ce0360c1f93b82d8ecf647/", Api.Controllers.LoaderController, :challenge
-
-  # pipeline :customer_api do
-  #   plug :accepts, ["json"]
-  #   plug Api.Plug.CustomerAuth
-  # end
-  # scope "/api/customer" do
-  #   pipe_through :customer_api
-  #   post "/graphql", Absinthe.Plug.GraphiQL,
-  #     schema: Api.GraphqlSchema,
-  #     interface: :simple
-  # end
+  get "/termos-de-uso", Api.CentralController, :terms_of_use
 
   pipeline :motoboy_api do
     plug :accepts, ["json"]
