1e16b39b69cbeef2351a2b0661d8211c6b7b2476
Rafael Garcia <rafbgarcia@gmail.com>
2018-02-02 11:30:23 -0200

    Box file and layout files

--- apps/db/config/dev.exs
+++ apps/db/config/dev.exs
@@ -1,17 +1,17 @@
 use Mix.Config
 
-# config :db, Db.Repo,
-#   adapter: Ecto.Adapters.Postgres,
-#   username: System.get_env("DATA_DB_USER"),
-#   password: System.get_env("DATA_DB_PASS"),
-#   hostname: System.get_env("DATA_DB_HOST"),
-#   database: "imoto_dev",
-#   pool_size: 10
-
 config :db, Db.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "rafa",
-  password: "",
+  username: System.get_env("DATA_DB_USER"),
+  password: System.get_env("DATA_DB_PASS"),
+  hostname: System.get_env("DATA_DB_HOST"),
   database: "imoto_dev",
-  hostname: "localhost",
   pool_size: 10
+
+# config :db, Db.Repo,
+#   adapter: Ecto.Adapters.Postgres,
+#   username: "rafa",
+#   password: "",
+#   database: "imoto_dev",
+#   hostname: "localhost",
+#   pool_size: 10
