fbacab588283394fa480be69e21ca86add06ca51
Rafael Garcia <rafbgarcia@gmail.com>
2018-02-21 23:35:44 -0300

    boxfile build prod

--- apps/db/config/dev.exs
+++ apps/db/config/dev.exs
@@ -1,12 +1,12 @@
 use Mix.Config
 
-config :db, Db.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  username: System.get_env("DATA_DB_USER"),
-  password: System.get_env("DATA_DB_PASS"),
-  hostname: System.get_env("DATA_DB_HOST"),
-  database: "imoto_dev",
-  pool_size: 10
+# config :db, Db.Repo,
+#   adapter: Ecto.Adapters.Postgres,
+#   username: System.get_env("DATA_DB_USER"),
+#   password: System.get_env("DATA_DB_PASS"),
+#   hostname: System.get_env("DATA_DB_HOST"),
+#   database: "imoto_dev",
+#   pool_size: 10
 
 config :db, Db.Repo,
   adapter: Ecto.Adapters.Postgres,
